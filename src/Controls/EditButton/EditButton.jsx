c04551f03c2f33512bc5f37f9d6880216dd36f0d
Islam Sayed <islam.sayed8@gmail.com>
2019-09-02 15:26:23 +0200

    component init

---
+++ src/Controls/EditButton/EditButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './EditButton.scss'
+
+const EditButton = (props) => {
+    return ( 
+        <button className="btn edit-btn">Edit</button>
+     );
+}
+ 
+export default EditButton;
