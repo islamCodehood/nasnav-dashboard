1e739e8226b2be00e352f2da471b828da002a6e2
Islam Sayed <islam.sayed8@gmail.com>
2019-09-02 07:22:23 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import DeleteButton from './Controls/DeleteButton/DeleteButton'
+function App() {
+  return (
+    <div className="App">
+      <DeleteButton />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Controls/DeleteButton/DeleteButton.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './DeleteButton.scss'
+
+const DeleteButton = (props) => {
+    return ( 
+        <button className="btn btn-default btn-sm">Small button</button>
+     );
+}
+ 
+export default DeleteButton;
