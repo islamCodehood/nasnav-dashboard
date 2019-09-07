4dfb20379aadc64113382050b2baa3e3d3c88b9c
Islam Sayed <islam.sayed8@gmail.com>
2019-09-07 17:23:33 +0200

    style: text-center class

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Logo = () => {
     return ( 
-        <div className="h1 col-2">
+        <div className="h1 col-2 text-center">
             LOGO
         </div>
      );
