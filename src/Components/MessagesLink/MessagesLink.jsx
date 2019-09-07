6975760d55eaa1d8512c5c8a33846bfce2c9385c
Islam Sayed <islam.sayed8@gmail.com>
2019-09-07 17:24:16 +0200

    style: increase width

--- src/Components/MessagesLink/MessagesLink.jsx
+++ src/Components/MessagesLink/MessagesLink.jsx
@@ -3,7 +3,7 @@ import MessageIcon from '../Icons/MessageIcon/MessageIcon'
 import './MessagesLink.scss'
 const MessagesLink = () => {
     return ( 
-        <div className="row no-gutters justify-content-around align-items-center message-notification">
+        <div className="row no-gutters mr-2 justify-content-around align-items-center message-notification">
                 <MessageIcon />
             <span>Messages</span>
         </div>
